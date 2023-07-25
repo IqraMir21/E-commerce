9fbfc54b56a5351fb8d1c03a130a37f593c77bd1
Halanoor <49690251+Halanoor@users.noreply.github.com>
2023-07-26 00:50:03 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ReadyToGo from "./componets/ReadyToGo";
+
+function App() {
+  return (
+    <div className="App">
+      <ReadyToGo />
+    </div>
+  );
+}
+
+export default App;
