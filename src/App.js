9e2cba4e6e5b221115a22beb147fdec6f217709b
Halanoor <49690251+Halanoor@users.noreply.github.com>
2023-08-01 23:28:16 +0500

    Adding cart pdf

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
 import "./App.css";
-// import GeneratePdf from "./componets/GeneratePdf";
-// import PfdGenerator from "./componets/PdfGenerator";
-// import ReadyToGo from "./componets/ReadyToGo";
-// import { jsPDF } from "jspdf";
+import GeneratePdf from "./componets/GeneratePdf";
+import ReadyToGo from "./componets/ReadyToGo";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <ReadyToGo />
-//       <PfdGenerator />
-//       <GeneratePdf />
-//     </div>
-//   );
-// }
-
-// export default App;
-import "./App.css";
-import JsPDF from "jspdf";
-const App = () => {
-  const generatePDF = () => {
-    const report = new JsPDF("portrait", "pt", "a4");
-    report.html(document.querySelector("#report")).then(() => {
-      report.save("report.pdf");
-    });
-  };
+function App() {
   return (
     <div className="App">
-      <button onClick={generatePDF} type="button">
-        Export PDF
-      </button>
+      <ReadyToGo />
+      <GeneratePdf />
     </div>
   );
-};
+}
 
 export default App;
--- src/componets/GeneratePdf.js
+++ src/componets/GeneratePdf.js
@@ -1,14 +1,36 @@
-import { useRef } from "react";
 import html2canvas from "html2canvas";
 import { jsPDF } from "jspdf";
+import React, { useRef, useCallback } from "react";
+// import { Bar } from "react-chartjs-2";
+import { Doughnut } from "react-chartjs-2";
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+
+const data = {
+  labels: ["Red", "Yellow", "Green", "Grey", "Dark Grey"],
+  datasets: [
+    {
+      label: "My Dataset",
+      data: [300, 50, 100, 40, 120],
+      backgroundColor: ["#FF6384", "#008000", "#FFFF00", "#ccc", "#DCDCDC"],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+Chart.register(CategoryScale);
 
 const GeneratePdf = () => {
   const inputRef = useRef(null);
+  // const input = document.getElementById("divToPrint");
   const printDocument = () => {
     html2canvas(inputRef.current).then((canvas) => {
-      const imgData = canvas.toDataURL("image/png");
-      const pdf = new jsPDF();
-      pdf.addImage(imgData, "JPEG", 0, 0);
+      let imgWidth = 208;
+      let imgHeight = (canvas.height * imgWidth) / canvas.width;
+      const imgData = canvas.toDataURL("img/png");
+      const pdf = new jsPDF("p", "mm", "a4");
+      pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+      // pdf.output("dataurlnewwindow");
       pdf.save("download.pdf");
     });
   };
@@ -17,12 +39,15 @@ const GeneratePdf = () => {
       <div className="App">
         <h1>Hello CodeSandbox</h1>
         <h2>Start editing to see some magic happen!</h2>
-        <div className="mb5">
-          <button onClick={printDocument}>Print</button>
-        </div>
         <div id="divToPrint" ref={inputRef}>
           <div>Note: Here the dimensions of div are same as A4</div>
           <div>You Can add any component here</div>
+          <div style={{ height: "690px", width: "690px", margin: "auto" }}>
+            <Doughnut data={data} />
+          </div>
+          <div className="mb5">
+            <button onClick={printDocument}>Print</button>
+          </div>
         </div>
       </div>
     </>
